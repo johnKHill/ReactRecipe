be62734e669a4cd88a8f15ad5e4e5759a0a80486
Johknotter Hill <johknotterhill@Johknotters-MacBook-Pro.local>
2020-04-18 11:01:58 -0400

    Delete button and method. OnClick method to remove form was also added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 import "./Navbar.css";
 
 class Navbar extends Component {
+
+  static defaultProps = {
+    onNewRecipe() {}
+  }
+
+  static propTypes = {
+    onNewRecipe: PropTypes.func
+  }
+
   render() {
     return (
       <header>
         <h2>Recipe App</h2>
         <nav>
-          <li><a href={`/#`}>New Recipe</a></li>
+          <li><a href={`/#`} onClick={this.props.onNewRecipe}>New Recipe</a></li>
           <li><a href={`/#`}>Home</a></li>
           <li><a href={`/#`}>About</a></li>
           <li><a href={`/#`}>Contact Us</a></li>
--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -13,7 +13,8 @@ class RecipeApp extends Component {
         {
           id: 0,
           title: "Spaghetti",
-          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          instructions:
+            "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
           ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
           img: "spaghetti.jpg",
         },
@@ -27,7 +28,8 @@ class RecipeApp extends Component {
         {
           id: 2,
           title: "Avocado Toast",
-          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          instructions:
+            "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
           ingredients: [
             "2 slices of bread",
             "1 avocado",
@@ -39,27 +41,41 @@ class RecipeApp extends Component {
         },
       ],
       nextRecipeId: 3,
+      showForm: false,
     };
 
     this.handleSave = this.handleSave.bind(this);
+    this.onDelete = this.onDelete.bind(this);
   }
 
   handleSave(recipe) {
     this.setState((prevState, props) => {
-      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      const newRecipe = { ...recipe, id: this.state.nextRecipeId };
       return {
         nextRecipeId: prevState.nextRecipeId + 1,
-        recipes: [...this.state.recipes, newRecipe]
-      }
+        recipes: [...this.state.recipes, newRecipe],
+        showForm: false,
+      };
     });
   }
-  render() {
 
+  onDelete(id) {
+    const recipes = this.state.recipes.filter(r => r.id !== id);
+    this.setState({recipes});
+  }
+
+  render() {
+    const { showForm } = this.state;
     return (
       <div className="App">
-        <Navbar />
-        <RecipeInput onSave={this.handleSave}/>
-        <RecipeList recipes={this.state.recipes}/>
+        <Navbar onNewRecipe={() => this.setState({ showForm: true })} />
+        {showForm ? (
+          <RecipeInput
+            onSave={this.handleSave}
+            onClose={() => this.setState({ showForm: false })}
+          />
+        ) : null}
+        <RecipeList onDelete={this.onDelete} recipes={this.state.recipes} />
       </div>
     );
   }
